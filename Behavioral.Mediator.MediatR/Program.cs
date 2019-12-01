c47c76cee71339aee9ee7b4cd3a32263b30f795a
dodinhnguyen <nguyendo891@yahoo.com>
2019-12-01 15:29:30 +0100

    Add project files.

---
+++ Behavioral.Mediator.MediatR/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Autofac;
+using JetBrains.Annotations;
+using MediatR;
+
+namespace Behavioral.Mediators.MediatR
+{
+    public class PongResponse
+    {
+        public DateTime Timestamp;
+
+        public PongResponse(DateTime timestamp)
+        {
+            Timestamp = timestamp;
+        }
+    }
+
+    public class PingCommand : IRequest<PongResponse>
+    {
+        // nothing here
+    }
+
+    [UsedImplicitly]
+    public class PingCommandHandler : IRequestHandler<PingCommand, PongResponse>
+    {
+        public async Task<PongResponse> Handle(PingCommand request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(new PongResponse(DateTime.UtcNow))
+              .ConfigureAwait(false);
+        }
+    }
+
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            _ = builder.RegisterType<Mediator>()
+              .As<IMediator>()
+              .InstancePerLifetimeScope(); // singleton
+
+            builder.Register<ServiceFactory>(context =>
+            {
+                var c = context.Resolve<IComponentContext>();
+                return t => c.Resolve(t);
+            });
+
+            builder.RegisterAssemblyTypes(typeof(Program).Assembly)
+              .AsImplementedInterfaces();
+
+            var container = builder.Build();
+            var mediator = container.Resolve<IMediator>();
+            var response = await mediator.Send(new PingCommand());
+            Console.WriteLine($"We got a pong at {response.Timestamp}");
+        }
+    }
+}
